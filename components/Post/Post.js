ee0794beb87c98eee8d491c865b994c3d63ceb36
Lisa Lu <lisalue@posteo.de>
2022-03-10 15:35:01 +0100

    update functions

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -1,19 +1,24 @@
+import Link from "next/link";
 import * as React from "react";
 import styled from "styled-components";
 
 export function Post({ id, title, date }) {
   return (
-    <Container>
-      <Title id={id}>{title}</Title>
-      <time dateTime={date}>{date}</time>
-    </Container>
+    <Link href={`/blog/${id}`}>
+      <a>
+        <Container>
+          <Title id={id}>{title}</Title>
+          <time dateTime={date}>{date}</time>
+        </Container>
+      </a>
+    </Link>
   );
 }
 
 const Container = styled.article`
   padding: 1rem;
-  border: 1px solid gray;
-  border-radius: 8px;
+  border: 2px dotted gray;
+  border-radius: 12px;
 `;
 
 const Title = styled.h3`
---
+++ posts/Hello Universe.md
@@ -0,0 +1,6 @@
+---
+title: "Hello Universe"
+date: "2020-01-01"
+---
+
+Yeah, we are in!
